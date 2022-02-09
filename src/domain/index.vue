bc6970399b11a0cf52a211b94b894b22605f2b0b
zhutisang <1137576510@qq.com>
2022-02-09 20:32:13 +0800

    first

---
+++ src/domain/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-layout>
+    <v-app-bar density="compact" absolute>
+      <v-app-bar-title>Vuetify</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text>Developer</v-btn>
+
+      <v-btn class="mr-3" text>Help</v-btn>
+
+      <v-divider inset vertical></v-divider>
+
+      <v-btn
+        class="mr-4 ml-5"
+        color="primary"
+        plain
+      >
+        <v-icon left icon="mdi-handshake-outline"></v-icon>
+
+        <span>John Leider</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-sheet border>
+        <v-container>
+          <section>
+            <h4 class="text-h4">Heading</h4>
+
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo consectetur quos velit inventore optio omnis, tempora nam officia, labore quibusdam error facere ipsum iste temporibus laboriosam aliquam quia vitae magni.
+            </div>
+          </section>
+        </v-container>
+      </v-sheet>
+    </v-main>
+  </v-layout>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop, Emit, Ref } from 'vue-property-decorator';
+@Component({
+
+})
+export default class domain extends Vue{
+
+}
+</script>
